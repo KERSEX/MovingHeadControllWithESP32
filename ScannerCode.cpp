0507303dc60f6e67d16cb4945926c67a099c24f7
WHO_KERS <152916692+KERSEX@users.noreply.github.com>
2025-05-02 20:17:03 +0200

    Add files via upload

--- ScannerCode.cpp
+++ ScannerCode.cpp
@@ -2,11 +2,11 @@
 #include <WiFiUdp.h>
 
 // KONFIGURATION - Ändern Sie diese Werte
-const char* ssid = "Dein_WiFi_Netzwerk";
-const char* password = "Dein_WiFi_Passwort";
-const char* udpAddress = "192.168.1.100";  // IP des PCs
-const int udpPort = 12345;                 // Port für die Kommunikation
-const int receiverID = #;                  // 1 für ersten Empfänger, 2 für zweiten
+const char* ssid = "Dein_WiFi_Netzwerk"; // Wifi Name
+const char* password = "Dein_WiFi_Passwort"; //Wifi Password
+const char* udpAddress = "192.168.1.100";  // IP from PC 
+const int udpPort = 12345;                 // Port for Communication
+const int receiverID = 1;                  // 1 for the first, 2 for the second
 
 WiFiUDP udp;
 
@@ -15,11 +15,11 @@ void setup() {
   WiFi.begin(ssid, password);
   
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Verbinde mit WiFi...");
+    delay(1000); // Just Delay 
+    Serial.println("Verbinde mit WiFi..."); // The ESP32 want WiFi Connection
   }
   
-  Serial.println("Verbunden mit WiFi");
+  Serial.println("Verbunden mit WiFi"); // The ESP32 is connectet with WiFi
   Serial.print("Receiver ID: ");
   Serial.println(receiverID);
   Serial.print("IP-Adresse: ");
@@ -37,6 +37,6 @@ void loop() {
   udp.print(message);
   udp.endPacket();
 
-  Serial.println(message);  // Debug-Ausgabe
-  delay(500);               // Alle 500ms senden
+  Serial.println(message);  // Debug Message
+  delay(500);               // All 0.5 Seconds a Signal 
 }
